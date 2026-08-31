936617659e7bff4c5a0853642d536f4e1471f53e
Anderson Ribeiro <ander_an10@hotmail.com>
2026-08-31 13:36:53 -0300

    Configura CSRF para Railway

--- saresp/settings.py
+++ saresp/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://projetoulysse-production.up.railway.app",
+]
 
 # ============================================================
 # APPLICATIONS
